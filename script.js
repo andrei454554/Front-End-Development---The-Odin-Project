24e7309fe0ed3dce81fd478cfa94c852dc4a4601
andrei454554 <andrei.a@niphoros.tech>
2023-04-23 13:19:36 -0700

    played around in script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+let selectedColors = ["red", 'blue'];
+selectedColors[2] = 1
+console.log(selectedColors.length);
+
+function greet(name, lastName) {
+    console.log('Hello' + name + '' + lastName)
+}
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(2)
+console.log(number)
+
+
